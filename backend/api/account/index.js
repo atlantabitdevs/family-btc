e295aada2015b250764e2aa5e1db0e8d8a7f6d24
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-05-07 17:42:52 -0400

    Fix imports and exports

--- backend/api/account/index.js
+++ backend/api/account/index.js
@@ -3,6 +3,9 @@ const router = express();
 
 const { getAccountAllowance, payInvoice, createInvoice } = require('./accountController');
 
+const { getBalance } = require('../../sensei/nodes');
+
+router.get('/account/balance', getBalance);
 router.get('/allowance', getAccountAllowance);
 router.get('/payment/send', payInvoice);
 router.get('/payment/receive', createInvoice);
