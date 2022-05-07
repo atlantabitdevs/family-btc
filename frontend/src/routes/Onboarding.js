cc83fe60bf360ce17f7b0035a48fb050d393b374
Stephen DeLorme <stephen@d.elor.me>
2022-05-07 18:33:15 -0400

    Cete name update

--- frontend/src/routes/Onboarding.js
+++ frontend/src/routes/Onboarding.js
@@ -6,16 +6,16 @@ const Onboarding = () => {
   return (
     <div className="p-8 space-y-8 min-h-screen flex flex-col align-center justify-center bg-mountains bg-cover">
       <div className="mb-16">
-        <h1 className="text-8xl lowercase font-display drop-shadow-xl font-[900] my-8">Tribe</h1>
+        <h1 className="text-8xl font-display drop-shadow-xl font-[900] my-8">Cete</h1>
 
         <p className="text-2xl">Finances for your people</p>
       </div>
       
       <Link to="/new-family">
-        <Button>Create a new tribe</Button>
+        <Button>Create a new cete</Button>
       </Link>
 
-      <Button style="free">Restore existing tribe</Button>
+      <Button style="free">Restore existing cete</Button>
       
       <p className="text-center drop-shadow-md pt-32">Made with ❤️ in the ATL</p>
     </div>
