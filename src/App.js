5cd056635ce64db8bd3952ad895fdf4d02a038a9
Alex Lewin <alexlwn123@gmail.com>
2022-05-07 00:20:20 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  Outlet,
+} from "react-router-dom";
 import Account from "./routes/Account";
+import Accounts from "./routes/Accounts";
 import Home from "./routes/Home";
+import Family from "./routes/Family";
 import NewAccount from "./routes/NewAccount";
+
+const Child = {};
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/admin">Admin</Link>
-        <Link to="account/BitDevs/Steven">Bitdevs/Steven</Link>
-        <Link to="account/BitDevs/Alex">Bitdevs/Alex</Link>
+        <NavLink to="/">Home</NavLink> |{" "}
       </nav>
 
       <Routes>
-        <Route path="/admin" />
-        <Route path="/account" element={<NewAccount />}>
-          <Route path="/account/:family/:user" element={<Account />} />
-          <Route path="/account/new" element={<NewAccount />} />
+        <Route path="/" element={<Home />}>
+          <Route path="family" element={<Family />}>
+            <Route path=":family" element={<Accounts />}>
+              <Route path=":account" element={<Account />} />
+            </Route>
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
-        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
