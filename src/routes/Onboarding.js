0973494825a88021ebe656f67af4d9a6fa22c60a
Stephen DeLorme <stephen@d.elor.me>
2022-05-06 23:38:11 -0400

    Added bitcoin icons

--- src/routes/Onboarding.js
+++ src/routes/Onboarding.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {BitcoinIcon} from '@bitcoin-design/bitcoin-icons-react/filled';
 
 const Onboarding = () => {
   return (
     <div>
       <h1>Onboarding Page</h1>
+      
+      <BitcoinIcon className="w-20 h-20 text-black" />
       <ul>
         <li>
           <Link to="/admin">Admin</Link>
